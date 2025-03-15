70d7da4f44f91d971b32dfd5bcfbcc4898b58528
martindipeco <martindipeco@gmail.com>
2025-03-15 19:16:12 -0300

    incluir apuntes en txt

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import functions.switchcase.chatgpt.ProductCategory;
 import functions.switchcase.chatgpt.SwitchDiscountCalculator;
 import functions.switchcase.claude.auxiliar.InvalidNotificationTypeException;
 import functions.switchcase.claude.auxiliar.Message;
+import functions.switchcase.claude.claudesolution.NotificationFactory;
 import functions.switchcase.claude.claudesolution.NotificationType;
 import functions.switchcase.claude.mysolution.InvalidTypeNotificationExeption;
 import functions.switchcase.claude.mysolution.NotificationFactoryImpl;
@@ -12,10 +13,6 @@ public class Main {
     public static void main(String[] args) throws InvalidNotificationTypeException, InvalidTypeNotificationExeption {
         System.out.println("welcome to MDP`s Uncle Bob Lessons");
 
-        NotificationService notificationService = new NotificationService();
-        notificationService.sendNotification(new Message(NotificationType.EMAIL, "This is an email"));
 
-        NotificationFactoryImpl notificationFactory = new NotificationFactoryImpl();
-        notificationFactory.createNotification(functions.switchcase.claude.mysolution.NotificationType.EMAIL);
     }
 }
