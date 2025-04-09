3f9c0b02e631944faad9bbe11fb0842b964523b7
martindipeco <martindipeco@gmail.com>
2025-04-09 08:28:40 -0300

    unchecked exceptions

---
+++ src/error_handling/exceptions/UseCheckedExceptionsInSpecificOcassions.java
@@ -0,0 +1,27 @@
+package error_handling.exceptions;
+
+import java.io.IOException;  // This is a checked exception
+
+public class UseCheckedExceptionsInSpecificOcassions {
+
+    // Note that the method declares the checked exception in its signature
+    public static void main(String[] args) {
+        try {
+            accessArrayElement(5);
+        } catch (IOException e) {
+            System.out.println("Caught exception: " + e.getMessage());
+        }
+    }
+
+    // This method throws a checked exception
+    public static void accessArrayElement(int index) throws IOException {
+        int[] numbers = {1, 2, 3};
+
+        if (index >= 0 && index < numbers.length) {
+            System.out.println(numbers[index]);
+        } else {
+            // Instead of throwing an unchecked exception, we throw a checked one
+            throw new IOException("Invalid array index: " + index);
+        }
+    }
+}
---
+++ src/error_handling/exceptions/UseUncheckedExceptions.java
@@ -0,0 +1,30 @@
+package error_handling.exceptions;
+
+public class UseUncheckedExceptions {
+
+    private static int[] numbers = {1, 2, 3};
+
+    public static void main(String[] args) {
+        //option 1
+        checkArrayBoundsBeforeAccessing(5);
+
+        //option 2
+        useTryCatch(5);
+    }
+
+    public static void checkArrayBoundsBeforeAccessing(int index) {
+        if (index >= 0 && index < numbers.length) {
+            System.out.println(numbers[index]);
+        } else {
+            System.out.println("Index out of bounds: " + index);
+        }
+    }
+
+    public static void useTryCatch(int index) {
+        try {
+            System.out.println(numbers[index]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Caught exception: " + e.getMessage());
+        }
+    }
+}
